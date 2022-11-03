ed141178e183c1143e95e5c65b6590bb7c67be86
veeru371 <84440176+veeru371@users.noreply.github.com>
2022-11-03 19:10:09 +0530

    #include<bits/stdc++.h> using namespace std; int main(){     int a;     cin >>a;     for(int i=1;i<=a;i++){         int b,c;         cin >>b>>c;         cout << b+c << " ";     } }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a;
+    cin >>a;
+    for(int i=1;i<=a;i++){
+        int b,c;
+        cin >>b>>c;
+        cout << b+c << "
+";
+    }
+}
