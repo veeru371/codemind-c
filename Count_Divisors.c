44233c8fa20ead5d2edbdb89ef2aa1feb1435c78
veeru371 <84440176+veeru371@users.noreply.github.com>
2022-11-04 12:49:26 +0530

    #include<bits/stdc++.h> using namespace std; int main(){     int a,b,c,cnt=0;     cin >>a>>b>>c;     for (int i=a;i<=b;i++){         if(i%c==0){             cnt+=1;         }     }     cout << cnt; }

---
+++ Count_Divisors.c
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b,c,cnt=0;
+    cin >>a>>b>>c;
+    for (int i=a;i<=b;i++){
+        if(i%c==0){
+            cnt+=1;
+        }
+    }
+    cout << cnt;
+}
